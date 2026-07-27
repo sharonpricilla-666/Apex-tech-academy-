9f5c3873c997a89188ce3f8c24d8e37ad5ddc7e1
sharonpricilla-666 <sharonpricilla6@gmail.com>
2026-07-27 22:13:26 +0530

    Create login.js

---
+++ js/js/login.js
@@ -0,0 +1,26 @@
+import {
+  auth,
+  signInWithEmailAndPassword
+} from "./firebase.js";
+
+const loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+  loginForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+
+      alert("Login Successful!");
+
+      window.location.href = "dashboard.html";
+
+    } catch (error) {
+      alert(error.message);
+    }
+  });
+}
